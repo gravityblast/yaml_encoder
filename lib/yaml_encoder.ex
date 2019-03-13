b05f2f2097b4d127aa6e52fd646705c74c7354f5
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-03-13 11:53:50 +0100

    Run formatter, allow encoding empty lists

--- lib/yaml_encoder.ex
+++ lib/yaml_encoder.ex
@@ -6,8 +6,8 @@ defmodule YamlEncoder do
   @doc """
   Encodes data to YAML
   """
-  def encode value do
-    encode 0, value
+  def encode(value) do
+    encode(0, value)
   end
 
   defp encode(indent, data) when is_number(data) do
@@ -15,86 +15,93 @@ defmodule YamlEncoder do
   end
 
   defp encode(indent, data) when is_boolean(data) do
-    value = case data do
-      true -> "true"
-      _ -> "false"
-    end
+    value =
+      case data do
+        true -> "true"
+        _ -> "false"
+      end
+
     ~s(#{value}\n)
   end
 
   defp encode(indent, data) when is_binary(data) do
-    value = encode_string indent, data
+    value = encode_string(indent, data)
     ~s(#{value}\n)
   end
 
   defp encode(indent, data) when is_atom(data) do
-    encode indent, to_string(data)
+    encode(indent, to_string(data))
+  end
+
+  defp encode(indent, []) do
+    "[]\n"
   end
 
   defp encode(indent, data) when is_list(data) do
-    encode_list indent, "", data
+    encode_list(indent, "", data)
   end
 
   defp encode(indent, {k, v}) do
-    prefix = indent_spaces indent
-    value = encode indent, v
+    prefix = indent_spaces(indent)
+    value = encode(indent, v)
     "#{prefix}#{k}: #{value}"
   end
 
   defp encode(indent, data) when is_map(data) do
-    encode_map indent, "", data
+    encode_map(indent, "", data)
   end
 
-  defp encode_list(indent, s, [head|tail]) when is_tuple(head) do
-    for {k, v} <- [head|tail],
-      do: encode_key_value(indent, {k, v}),
-      into: s
+  defp encode_list(indent, s, [{_, _} | _] = list) do
+    for {k, v} <- list,
+        do: encode_key_value(indent, {k, v}),
+        into: s
   end
 
-  defp encode_list indent, s, [head|tail] do
-    value = encode_list_item indent, head
-    encode_list indent, "#{s}#{value}", tail
+  defp encode_list(indent, s, [head | tail]) do
+    value = encode_list_item(indent, head)
+    encode_list(indent, "#{s}#{value}", tail)
   end
 
-  defp encode_list indent, s, [] do
+  defp encode_list(indent, s, []) do
     s
   end
 
-  defp encode_list_item indent, data do
-    value = encode data
+  defp encode_list_item(indent, data) do
+    value = encode(data)
     prefix = indent_spaces(indent)
     "#{prefix}- #{value}"
+
     if is_map(data) || is_list(data) do
-      value = encode indent + 1, data
+      value = encode(indent + 1, data)
       "#{prefix}-\n#{value}"
     else
-      value = encode indent, data
+      value = encode(indent, data)
       "#{prefix}- #{value}"
     end
   end
 
-  defp encode_map indent, s, data do
+  defp encode_map(indent, s, data) do
     for {k, v} <- data,
-      do: encode_key_value(indent, {k, v}),
-      into: s
+        do: encode_key_value(indent, {k, v}),
+        into: s
   end
 
-  defp encode_key_value indent, {k, v} do
-    prefix = indent_spaces indent
+  defp encode_key_value(indent, {k, v}) do
+    prefix = indent_spaces(indent)
 
-    if is_map(v) || is_list(v) do
-      value = encode indent + 1, v
+    if is_map(v) || (is_list(v) && length(v) > 0) do
+      value = encode(indent + 1, v)
       "#{prefix}#{k}:\n#{value}"
     else
-      value = encode indent, v
+      value = encode(indent, v)
       "#{prefix}#{k}: #{value}"
     end
   end
 
-  defp encode_string indent, data do
+  defp encode_string(indent, data) do
     single_quotes = data =~ ~r/'/
     double_quotes = data =~ ~r/"/
-    encode_string indent, data, single_quotes, double_quotes
+    encode_string(indent, data, single_quotes, double_quotes)
   end
 
   defp encode_string(indent, data, true, true) do
@@ -109,11 +116,11 @@ defmodule YamlEncoder do
     ~s("#{data}")
   end
 
-  defp indent_spaces 0 do
+  defp indent_spaces(0) do
     ""
   end
 
-  defp indent_spaces n do
-    for _ <- (0..n-1), do: "  ", into: ""
+  defp indent_spaces(n) do
+    for _ <- 0..(n - 1), do: "  ", into: ""
   end
 end
--- test/yaml_encoder_test.exs
+++ test/yaml_encoder_test.exs
@@ -3,79 +3,98 @@ defmodule YamlEncoderTest do
   doctest YamlEncoder
 
   test "encode number" do
-    value = YamlEncoder.encode 1
+    value = YamlEncoder.encode(1)
     assert value == "1\n"
   end
 
   test "encode boolean" do
-    value = YamlEncoder.encode true
+    value = YamlEncoder.encode(true)
     assert value == "true\n"
 
-    value = YamlEncoder.encode false
+    value = YamlEncoder.encode(false)
     assert value == "false\n"
   end
 
   test "encode string" do
-    value = YamlEncoder.encode "foo"
+    value = YamlEncoder.encode("foo")
     assert value == ~s("foo"\n)
   end
 
   test "encode atom" do
-    value = YamlEncoder.encode :foo
+    value = YamlEncoder.encode(:foo)
     assert value == ~s("foo"\n)
   end
 
   test "encode tuple" do
-    value = YamlEncoder.encode {:foo, :bar}
+    value = YamlEncoder.encode({:foo, :bar})
     assert value == ~s(foo: "bar"\n)
   end
 
   test "encode list of tuples" do
-    value = YamlEncoder.encode [{:foo, 1}, {:bar, 2}]
+    value = YamlEncoder.encode([{:foo, 1}, {:bar, 2}])
+
     expected = """
-foo: 1
-bar: 2
-"""
+    foo: 1
+    bar: 2
+    """
+
     assert value == expected
   end
 
   test "encode string with single quotes" do
-    value = YamlEncoder.encode "foo 'bar'"
+    value = YamlEncoder.encode("foo 'bar'")
     assert value == ~s("foo 'bar'"\n)
   end
 
   test "encode string with double quotes" do
-    value = YamlEncoder.encode ~s(foo "bar")
+    value = YamlEncoder.encode(~s(foo "bar"))
     assert value == ~s('foo "bar"'\n)
   end
 
   test "encode string with single and double quotes" do
-    value = YamlEncoder.encode ~s(foo "bar" 'baz')
+    value = YamlEncoder.encode(~s(foo "bar" 'baz'))
     assert value == ~s('''foo "bar" 'baz''''\n)
   end
 
   test "encode list" do
-    value = YamlEncoder.encode [1, "foo", 2, "bar"]
+    value = YamlEncoder.encode([1, "foo", 2, "bar"])
+
     expected = """
-- 1
-- "foo"
-- 2
-- "bar"
-"""
+    - 1
+    - "foo"
+    - 2
+    - "bar"
+    """
+
+    assert value == expected
+  end
+
+  test "encode empty list" do
+    value = YamlEncoder.encode([])
+
+    expected = "[]\n"
+    assert value == expected
+  end
+
+  test "encode nested empty list" do
+    value = YamlEncoder.encode([{:foo, []}])
+
+    expected = "foo: []\n"
     assert value == expected
   end
 
   test "encode map" do
-    value = YamlEncoder.encode %{"foo" => 1, "bar" => "baz"}
+    value = YamlEncoder.encode(%{"foo" => 1, "bar" => "baz"})
+
     expected_lines = [
       ~s(bar: "baz"),
       ~s(foo: 1)
     ]
 
-    lines = String.split value, "\n"
+    lines = String.split(value, "\n")
 
     for expected <- expected_lines do
-      found = Enum.find lines, &(&1 == expected)
+      found = Enum.find(lines, &(&1 == expected))
       assert found
     end
   end
@@ -85,14 +104,14 @@ bar: 2
       "foo" => [1, 2, 3]
     }
 
-    value = YamlEncoder.encode data
+    value = YamlEncoder.encode(data)
 
     expected = """
-foo:
-  - 1
-  - 2
-  - 3
-"""
+    foo:
+      - 1
+      - 2
+      - 3
+    """
 
     assert value == expected
   end
@@ -110,6 +129,7 @@ foo:
                   [1, 2],
                   [3, 4]
                 ],
+                "x" => [],
                 "z" => [
                   {:a, 1},
                   {:b, 2}
@@ -121,29 +141,30 @@ foo:
       }
     }
 
-    value = YamlEncoder.encode data
+    value = YamlEncoder.encode(data)
 
     expected = """
-foo:
-  bar:
-    baz:
-      xxx:
-        - 1
-        - 2
-      zzz:
-        -
-          hello: 1
-          world:
+    foo:
+      bar:
+        baz:
+          xxx:
+            - 1
+            - 2
+          zzz:
             -
-              - 1
-              - 2
-            -
-              - 3
-              - 4
-          z:
-            a: 1
-            b: 2
-"""
+              hello: 1
+              world:
+                -
+                  - 1
+                  - 2
+                -
+                  - 3
+                  - 4
+              x: []
+              z:
+                a: 1
+                b: 2
+    """
 
     assert value == expected
   end
